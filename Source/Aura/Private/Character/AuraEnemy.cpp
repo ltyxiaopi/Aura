f382e60f8734e3930fcdbd49a39d0b1717dc225b
Ltykimodi <132186444+ltyxiaopi@users.noreply.github.com>
2025-06-14 08:57:25 +0800

    build attributes

--- Source/Aura/Private/Character/AuraEnemy.cpp
+++ Source/Aura/Private/Character/AuraEnemy.cpp
@@ -12,7 +12,7 @@ AAuraEnemy::AAuraEnemy() {
 	
 	AbilitySystemComponent = CreateDefaultSubobject<UAuraAbilitySystemComponent>("AbilitySystemComponent");
 	AbilitySystemComponent->SetIsReplicated(true);
-	
+	AbilitySystemComponent->SetReplicationMode(EGameplayEffectReplicationMode::Minimal);
 	AttributeSet = CreateDefaultSubobject<UAuraAttributeSet>("AttributeSet");
 }
 
@@ -26,4 +26,9 @@ void AAuraEnemy::HighlightActor() {
 void AAuraEnemy::UnHighlightActor() {
 	GetMesh()->SetRenderCustomDepth(false);
 	Weapon->SetRenderCustomDepth(false);
+}
+
+void AAuraEnemy::BeginPlay() {
+	Super::BeginPlay();
+	AbilitySystemComponent->InitAbilityActorInfo(this, this);
 }
