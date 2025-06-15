1765db6430b360b36bde320e161d5282b2f3ea8c
Ltykimodi <132186444+ltyxiaopi@users.noreply.github.com>
2025-06-15 23:54:04 +0800

    Build Overlay Widget Controller

--- Source/Aura/Private/UI/WidgetController/AuraWidgetController.cpp
+++ Source/Aura/Private/UI/WidgetController/AuraWidgetController.cpp
@@ -3,3 +3,9 @@
 
 #include "UI/WidgetController/AuraWidgetController.h"
 
+void UAuraWidgetController::SetWidgetControllerParams(const FWidgetControllerParams& WCParams) {
+	PlayerController = WCParams.PlayerController;
+	PlayerState = WCParams.PlayerState;
+	AbilitySystemComponnet = WCParams.AbilitySystemComponnet;
+	AttributeSet = WCParams.AttributeSet;
+}
---
+++ Source/Aura/Private/UI/WidgetController/OverlayWidgetController.cpp
@@ -0,0 +1,5 @@
+// Xiaopi
+
+
+#include "UI/WidgetController/OverlayWidgetController.h"
+
